de4043a21827ca968abb21f4db9139ef01b7221a
lolukuk <demogs1@gmail.com>
2024-03-05 18:28:22 +0700

    издержки

--- Stepik.org/levelcharm/level2.py
+++
@@ -1,8 +0,0 @@
-with open('input.txt', 'r') as f:
-    data = f.read()
-
-    list_of_letters = []
-    for letter in data:
-        list_of_letters.append(letter)
-
-    print(list_of_letters)
--- Stepik.org/levelcharm/main.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-# считывание списка из входного потока
-s = sys.stdin.readlines()
-lst_in = [list(map(int, x.strip().split())) for x in s]
-
-# Проверка симметричности относительно главной диагонали
-print(["НЕТ", "ДА"][is_symmetric := all(lst_in[i][j] == lst_in[j][i] for i in range(len(lst_in)) for j in range(len(lst_in)))])
