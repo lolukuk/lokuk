e497f7e6960978baca4299895673bbcfcd2e4a48
lolukuk <demogs1@gmail.com>
2024-03-16 19:21:53 +0700

    Stepik.org/

--- Stepik.org/levelcharm/level1.py
+++ Stepik.org/levelcharm/level1.py
@@ -1,2 +1,13 @@
-d = dict((c.split('=') for c in open(0)))
-print(*sorted(d, key=lambda x: int(d[x]), reverse=True))
+Проверка на включенность (1): if flags & mask == mask: # Бит включен (1)
+
+Выключение бита: flags & ~mask
+
+Включение бита: flags | mask
+
+Переключение бита: flags ^ mask
+
+Инверсия бит: ~number == -number - 1
+
+Смещение бит вправо: number >> 1 == number // (2 ** 1)
+
+Смещение бит влево: number << 1 == number * (2 ** 1)
--- Stepik.org/levelcharm/main.py
+++ Stepik.org/levelcharm/main.py
@@ -1,8 +1,35 @@
-import sys
+def can_make_potion(graph, ingredients, X, Y, S, visited):
+    if visited[S]:
+        return False
+    visited[S] = True
+    if S == 1:
+        return True
+    for ingredient in graph[S]:
+        required_ingredient = ingredients[ingredient]
+        new_X = X - required_ingredient[0]
+        new_Y = Y - required_ingredient[1]
+        if new_X >= 0 and new_Y >= 0 and can_make_potion(graph, ingredients, new_X, new_Y, ingredient, visited):
+            return True
+    return False
 
-# считывание списка из входного потока
-s = sys.stdin.readlines()
-lst_in = [list(map(int, x.strip().split())) for x in s]
+def main():
+    N = int(input())
+    graph = {}
+    ingredients = {}
+    for i in range(N - 2):
+        parts = list(map(int, input().split()))
+        K = parts[0]
+        ingredients[i + 3] = sum([ingredients.get(parts[j], []) for j in range(1, K + 1)], [])
+        for j in range(1, K + 1):
+            if parts[j] not in graph:
+                graph[parts[j]] = []
+            graph[parts[j]].append(i + 3)
 
-# Проверка симметричности относительно главной диагонали
-print(["НЕТ", "ДА"][is_symmetric := all(lst_in[i][j] == lst_in[j][i] for i in range(len(lst_in)) for j in range(len(lst_in)))])
+    Q = int(input())
+    for _ in range(Q):
+        X, Y, S = map(int, input().split())
+        visited = [False] * (N + 1)
+        print(1 if can_make_potion(graph, ingredients, X, Y, S, visited) else 0)
+
+if __name__ == "__main__":
+    main()
